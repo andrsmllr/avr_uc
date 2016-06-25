571e6f2b011c2bcc4d3d76daad4a77050c864da2
Andreas Mueller <andym2207@gmx.de>
2016-06-25 12:40:04 +0200

    Complete echo functionality and add welcome message

--- uart_echo_test/uart_echo_test.c
+++ uart_echo_test/uart_echo_test.c
@@ -8,7 +8,8 @@
  * 
  * \brief Simple test of UART on ATMEGA8.
  * 
- * \details ...
+ * \details On power-up send welcome message, then continue to echo
+ *          whatever is received by the UART.
  * 
  * \see ...
  */
@@ -22,20 +23,45 @@
 
 int main(int argc, char** argv)
 {
-    UBRRH = (uint8_t)((F_CPU/(16*BAUD_RATE))>>8); // Set baud rate.
-    UBRRL = (uint8_t)((F_CPU/(16*BAUD_RATE)));
-    UCSRB |= (1<<RXEN)|(1<<TXEN); // Enable rx and tx.
-    UCSRC |= (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
-    //~(1<<USBS)~(1<<UMSEL)
-    // Main loop.
+    char msg [12] = "Hello UART!\n";
+    int i = 0;
+    uint8_t rxd = 0;
+    
+    UBRRH = (uint8_t)((F_CPU / (16 * BAUD_RATE)) >> 8); // Set baud rate.
+    UBRRL = (uint8_t)((F_CPU / (16 * BAUD_RATE)));
+    UCSRB |= (1<<RXEN) | (1<<TXEN); // Enable rx and tx.
+    UCSRC |= (1<<URSEL) | (1<<UCSZ1) | (1<<UCSZ0); // 8 data bits, 1 stop bit.
+    
+    for (i = 0; i < 12; i++)
+    {
+        wait_uart_tx_ready();
+        UDR = msg[i]; // Send test message.
+    }
+    
+    _delay_ms(1000); // Wait.
+    
+    // Echo loop. Echo each received frame/character immediately (no buffering).
     while (1)
     {
-        while (!(UCSRA & (1<<UDRE)))
-        {} // Wait for tx register ready.
-        UDR = 0xFF;
-        _delay_ms(1000);
+        wait_uart_rx_avail();
+        rxd = UDR;
+        wait_uart_tx_ready();
+        UDR = rxd;
     }
     
     return 0;
 }
 
+void wait_uart_tx_ready() 
+{
+    while (!(UCSRA & (1<<UDRE)))
+    {} // Wait for tx register ready.
+    return;
+}
+
+void wait_uart_rx_avail()
+{
+    while (!(UCSRA & (1<<RXC)))
+    {} // Wait until rx data available.
+    return;
+}
--- uart_echo_test/uart_echo_test.h
+++ uart_echo_test/uart_echo_test.h
@@ -14,12 +14,17 @@
  */
 /**********************************************************************/
 
+#include <avr/io.h>
+
 #ifndef F_CPU
 #define F_CPU (16000000UL) ///< CPU frequency.
 #endif // F_CPU
 
 #ifndef UART_ECHO_TEST_H_
 #define UART_ECHO_TEST_H_
+
+void wait_uart_tx_ready();
+void wait_uart_rx_avail();
 
 #endif // UART_ECHO_TEST_H_
 
